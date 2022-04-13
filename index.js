8786dd739abb6ef74b9db50d58da7d79fb1c6b48
Tyler Mills <tmills9208@gmail.com>
2022-04-13 02:06:45 -0400

    init, mysql working and established connection

--- index.js
+++ index.js
@@ -1,8 +1,28 @@
 require('dotenv').config();
 const http = require('http');
+const mysql = require('mysql2');
 
 const host = process.env.HOST;
 const port = process.env.PORT;
+
+const sqlHost = process.env.MYSQL_HOST;
+const sqlPort = process.env.MYSQL_PORT;
+const sqlUser = process.env.MYSQL_USER;
+const sqlPass = process.env.MYSQL_PASS;
+
+const dbConnection = mysql.createConnection({
+  host: sqlHost,
+  port: sqlPort,
+  user: sqlUser,
+  password: sqlPass
+})
+
+// failed at first, needed to replace package: mysql -> mysql2
+// explanation of why here: https://stackoverflow.com/a/56509065
+dbConnection.connect(function(err) {
+  if (err) throw err;
+  console.log("MySQL Connection successful!");
+})
 
 const requestListener = function(req, res) {
   res.statusCode = 200;
